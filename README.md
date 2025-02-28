175702534071e55a674cf9d6cb7d654d227e30d7
Karthik <karthik@INPC0121.local>
2025-02-28 21:32:44 +0530

    create test app

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # simpleApi
 This repo is for creating a sample API 
+
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"username":"karthik"}' \
+  http://localhost:3000/login
+
+curl --header "Content-Type: application/json" --header "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImthcnRoaWsiLCJpYXQiOjE3NDA3NTQzNTIsImV4cCI6MTc0MDc1Nzk1Mn0.XcMpAudol221jC7LGz28AhJBO6UGdED71WyfS3zEr3E" --request GET http://localhost:3000/hello
+
+
+export SECRET_KEY=my-secret-key
+serverless deploy
+
---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const awsServerlessExpress = require('aws-serverless-express');
+
+
+const app = express();
+const SECRET_KEY = process.env.SECRET_KEY;
+app.use(express.json());
+
+// Verify JWT
+function authenticateToken(req, res, next) {
+    const token = req.header('Authorization')?.split(' ')[1];
+    if (!token) return res.status(401).json({ message: 'Access Denied! Token MIssing!' });
+
+    jwt.verify(token, SECRET_KEY, (err, user) => {
+        if (err) return res.status(403).json({ message: 'Invalid Token, Generate the new token!' });
+        req.user = user;
+        next();
+    });
+}
+
+// Public route to generate a token
+app.post('/login', (req, res) => {
+    const { username } = req.body;
+    if (!username) return res.status(400).json({ message: 'Username is required' });
+
+    const token = jwt.sign({ username }, SECRET_KEY, { expiresIn: '1h' });
+    res.status(200).json({ token });
+});
+
+// Protected route
+app.get('/hello', authenticateToken, (req, res) => {
+    res.status(200).json({ message: 'Hello, World!' });
+});
+
+// To run locally
+// app.listen(3000, () => {
+//     console.log(`Server running on port ${3000}`);
+// });
+
+// To deploy on AWS
+const server = awsServerlessExpress.createServer(app);
+exports.handler = async (event, context) => {
+    return awsServerlessExpress.proxy(server, event, context);
+};
